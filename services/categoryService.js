379d53ce856d50dd79a61c937e4f82f06076e0fd
ahmedhasan689 <ahm19edhasan@gmail.com>
2025-07-13 13:03:40 +0300

    Feat ( Category ) : Get All Categories API

--- services/categoryService.js
+++ services/categoryService.js
@@ -2,10 +2,17 @@ const CategoryModel = require("../models/categoryModel");
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 
-exports.getAllCategories = (req, res) => {
-  res.send();
-};
+// @desc    Create Category
+// @route   GET /api/v1/categories/get-all-categories
+// @access  Public
+exports.getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await CategoryModel.find({});
+  res.status(200).json({ results: categories.length, data: categories });
+});
 
+// @desc    Create Category
+// @route   POST /api/v1/categories/create
+// @access  Private
 exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
 
