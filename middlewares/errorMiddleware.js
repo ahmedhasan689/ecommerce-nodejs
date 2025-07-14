42da36b3467544d12c20f47c9c622fd623bf55c5
ahmedhasan689 <ahm19edhasan@gmail.com>
2025-07-14 10:46:37 +0300

    Fix ( Cors ) : cors config

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,11 +2,26 @@ const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
+  if (process.env.NODE_ENV === "development") {
+    sendErrorToDev(err, res);
+  } else {
+    sendErrorToProd(err, res);
+  }
+};
+
+const sendErrorToDev = (err, res) => {
+  return res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
     stack: err.stack,
+  });
+};
+
+const sendErrorToProd = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 };
 
